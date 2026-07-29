1c888121347bfba3d19ed0765c0f28e147d5b874
y2kzn <verdebufalo@gmail.com>
2026-07-29 17:37:05 -0300

    Update news.controller.js

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -12,81 +12,9 @@ class controller {
             
             return res.json([
   {
-    "id": 13829,
-    "headerText": "Zantares 即将到来",
-    "contentText": "腐化正在各个活动中蔓延……准备加入战斗。立即查看",
-    "language": "ZH-CN",
-    "newsType": "1",
-    "imagePath": "/file/get/zantares-teaser-news.png",
-    "targetUrl": "https://bit.ly/zantaresteaser",
-    "timestamp": "2026-03-25T16:15:49.1763524Z",
-    "startsAt": "2026-03-25T10:00:00Z",
-    "endsAt": "2026-03-31T10:00:00Z",
-    "allowedPlatforms": [],
-    "excludedPlatforms": [
-      "XBOX",
-      "PLAYSTATION",
-      "SWITCH"
-    ]
-  },
-  {
-    "id": 13828,
-    "headerText": "Zantares komt eraan",
-    "contentText": "Corruptie verspreidt zich over de events… Maak je klaar om mee te vechten. Bekijk het nu",
-    "language": "NL",
-    "newsType": "1",
-    "imagePath": "/file/get/zantares-teaser-news.png",
-    "targetUrl": "https://bit.ly/zantaresteaser",
-    "timestamp": "2026-03-25T16:15:49.1763524Z",
-    "startsAt": "2026-03-25T10:00:00Z",
-    "endsAt": "2026-03-31T10:00:00Z",
-    "allowedPlatforms": [],
-    "excludedPlatforms": [
-      "XBOX",
-      "PLAYSTATION",
-      "SWITCH"
-    ]
-  },
-  {
-    "id": 13827,
-    "headerText": "Zantares geliyor",
-    "contentText": "Yozlaşma etkinliklere yayılıyor… Savaşa katılmaya hazır ol. Hemen göz at",
-    "language": "TR",
-    "newsType": "1",
-    "imagePath": "/file/get/zantares-teaser-news.png",
-    "targetUrl": "https://bit.ly/zantaresteaser",
-    "timestamp": "2026-03-25T16:15:49.1763524Z",
-    "startsAt": "2026-03-25T10:00:00Z",
-    "endsAt": "2026-03-31T10:00:00Z",
-    "allowedPlatforms": [],
-    "excludedPlatforms": [
-      "XBOX",
-      "PLAYSTATION",
-      "SWITCH"
-    ]
-  },
-  {
-    "id": 13826,
-    "headerText": "Zantares is coming",
-    "contentText": "Corruption is spreading across events… Get ready to fight. Check it out now",
-    "language": "EN",
-    "newsType": "1",
-    "imagePath": "/file/get/zantares-teaser-news.png",
-    "targetUrl": "https://bit.ly/zantaresteaser",
-    "timestamp": "2026-03-25T16:15:49.1763524Z",
-    "startsAt": "2026-03-25T10:00:00Z",
-    "endsAt": "2026-03-31T10:00:00Z",
-    "allowedPlatforms": [],
-    "excludedPlatforms": [
-      "XBOX",
-      "PLAYSTATION",
-      "SWITCH"
-    ]
-  },
-  {
     "id": 13126,
     "headerText": "News",
-    "contentText": ".gg/stumblefusion",
+    "contentText": ".gg/stumbleboxer",
     "language": "EN",
     "newsType": "1",
     "imagePath": "/file/get/menuBG.png",
